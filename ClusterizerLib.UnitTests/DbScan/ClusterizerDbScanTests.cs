99cef1dfa2ecac37c50d815c21b7515ab327e732
berreip <pierre.bony@hotmail.fr>
2023-10-04 18:48:48 +0200

    first unit tests

---
+++ ClusterizerLib.UnitTests/DbScan/ClusterizerDbScanTests.cs
@@ -0,0 +1,19 @@
+﻿using ClusterizerLib.DbScan;
+
+namespace ClusterizerLib.UnitTests.DbScan;
+
+[TestFixture]
+internal sealed class ClusterizerDbScanTests
+{
+    [Test]
+    public void empty_clustering()
+    {
+        //Arrange
+
+        //Act
+        var res = ClusterizerDbScan.Run(Array.Empty<IPoint>());
+
+        //Assert
+        Assert.AreEqual(0, res.Count);
+    }
+}
